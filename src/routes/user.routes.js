b4c613a9879340d3982d73485b8305f49b70a683
Siddharth-Chaudhary1 <chsiddharth074@gmail.com>
2024-07-24 17:59:51 +0530

    add new route for controllers

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js"
+import { 
+        loginUser,
+        logoutUser,
+        registerUser,
+        refreshAccessToken,
+        changeCurrentPassword,
+        getCurrentUser,
+        updateAccountDetails,
+        updateUserAvatar,
+        updateUserCoverImage,
+        getUserChannelProfile,
+        getWatchHistory 
+} from "../controllers/user.controller.js"
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -26,5 +38,15 @@ router.route("/login").post(loginUser)
 // secured routes
 router.route("/logout").post(verifyJWT,logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/changed-password").post(verifyJWT,changeCurrentPassword)
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+router.route("/update-account").patch(verifyJWT,updateAccountDetails)
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+router.route("/cover-Image").patch(verifyJWT,upload.single("/coverImage"),updateUserCoverImage)
+
+// since we are taking data from params so we need this syntex (: this is important /c/ can be given any name)
+// since we have set default value as username hence we need to use "username" here
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+router.route("/history").get(verifyJWT,getWatchHistory)
 
 export default router
